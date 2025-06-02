92d9810edab43a8d6d8bede9827455dd3733d16d
Yashraj144 <ankitakedar.riyality@gmail.com>
2025-06-02 09:06:46 +0530

    Added Successfully

---
+++ Competitive-Exam-Management-Api/src/main/java/com/competitive_exam_management/Competitive_Exam_Management/CompetitiveExamManagementApplication.java
@@ -0,0 +1,13 @@
+package com.competitive_exam_management.Competitive_Exam_Management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompetitiveExamManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompetitiveExamManagementApplication.class, args);
+	}
+
+}
